9868c937833fd05e84a93382fd2a7401676e5c9f
bankacem <38163728+bankacem@users.noreply.github.com>
2026-01-11 21:17:21 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// WARNING: Storing passwords directly in client-side JavaScript is INSECURE and NOT recommended for production applications.
-// This is for demonstration purposes only.
-const config = {
-    password: "0600231590" // This password is for demonstration purposes only and is not secure.
-};
-
-export default config;
